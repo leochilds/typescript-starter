7317c4bc932059864a1d6d0f6490060998cd6e81
Leo Childs <ljochilds@gmail.com>
2021-11-09 15:14:37 +0000

    fix onchange for windows

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 const symDiff = (arrOne: number[], arrTwo: number[]): number[] => {
     const diff = arrOne.filter((num)=> !arrTwo.includes(num));
     diff.push(...arrTwo.filter((num)=> !arrOne.includes(num)));
-    return [...new Set(diff)].sort();
+    return [...new Set(diff)];
 };
 
 export const sym = (...args: number[][]): number[] => {
-    return args.reduce(symDiff);
+    return args.reduce(symDiff).sort();
 };
 
 console.info(sym([1, 2, 3], [5, 2, 1, 4]));
